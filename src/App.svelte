215fd5e498284038c4f8788fd4203ec16ca7c962
Riza Fahmi <rizafahmi@gmail.com>
2020-09-28 11:10:54 +0700

    Add Login page

--- src/App.svelte
+++ src/App.svelte
@@ -1,11 +1,5 @@
 <script>
-	export let name;
+  import Login from './Login.svelte';
 </script>
 
-<style>
-	h1 {
-		color: purple;
-	}
-</style>
-
-<h1>Hello {name}!</h1>
+<Login />
---
+++ src/Login.svelte
@@ -0,0 +1,19 @@
+<script>
+  let username = '';
+  let password = '';
+
+  async function handleSubmit() {}
+</script>
+
+<h1>Login</h1>
+<form on:submit|preventDefault={handleSubmit}>
+  <fieldset>
+    <label for="username">Username</label>
+    <input type="input" bind:value={username} placeholder="your username" />
+    <label for="password">Password</label>
+    <input type="password" bind:value={password} placeholder="password" />
+    <hr />
+    <button type="submit">Login</button>
+    <button on:click={handlRegisterClick} type="button">Register</button>
+  </fieldset>
+</form>
